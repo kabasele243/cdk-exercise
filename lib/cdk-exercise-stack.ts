2e0d20cf7e3424a4c3b59192f625c7e8d4d9c4ce
Franck Kabasele <kabasele467@gmail.com>
2022-02-24 23:38:52 -0500

    first commit

--- lib/cdk-exercise-stack.ts
+++ lib/cdk-exercise-stack.ts
@@ -6,11 +6,6 @@ export class CdkExerciseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkExerciseQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
